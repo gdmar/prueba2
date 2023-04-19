58fe2b927fe10132023dc5bd92782db07e23d30e
gabriel <gabriel@DESKTOP-AC507SB>
2023-04-19 10:37:02 -0300

    segundo intento

---
+++ tp2_grupo17-1prueba-2html/src/main/java/ar/edu/unju/fi/Tp2Grupo171prueba2htmlApplication.java
@@ -0,0 +1,13 @@
+package ar.edu.unju.fi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tp2Grupo171prueba2htmlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tp2Grupo171prueba2htmlApplication.class, args);
+	}
+
+}
